c5e9587a1e92180f018a290fa10dfd5340b9ad2b
James Meakin <code@jmsmkn.com>
2019-05-07 15:52:34 +0200

    Adds calls and image api implementation

--- gcapi/gcapi.py
+++ gcapi/gcapi.py
@@ -1,6 +1,35 @@
 # -*- coding: utf-8 -*-
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
 
 from requests import Session
+
+
+class APIBase:
+    base_path = ""
+
+    def __init__(self, client):
+        self._client = client
+
+    def list(self):
+        return self._client(method="GET", path=self.base_path)
+
+    def detail(self, pk):
+        return self._client(method="GET", path=urljoin(self.base_path, pk))
+
+
+class ImagesAPI(APIBase):
+    base_path = "cases/images"
+
+    def list(self):
+        # TODO
+        raise NotImplementedError
+
+
+class WorkstationSessionsAPI(APIBase):
+    base_path = "workstations/sessions"
 
 
 class Client(Session):
@@ -22,3 +51,18 @@ class Client(Session):
 
         # Should we verify the servers SSL certificates?
         self._verify = verify
+
+        self.images = ImagesAPI(client=self)
+        self.sessions = WorkstationSessionsAPI(client=self)
+
+    def __call__(self, method="GET", path=""):
+        response = self.request(
+            method=method,
+            url=urljoin(self._base_url, path),
+            headers=self.headers,
+            verify=self._verify,
+        )
+
+        response.raise_for_status()
+
+        return response.json()
