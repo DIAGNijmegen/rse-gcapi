585731731c1abccf0b2eecae8633e6e92fdbed13
Paul K. Gerke <pkcakeout@gmail.com>
2019-08-14 10:22:00 +0200

    Json schemas now are loaded during import, fixed modify-json
    * Json schemas are now loaded during the initial import stage
    * Missing json schems will raise an ImportError to signal a library failure
    * Fixed the ModifiableMixin json verification. Now should actually do something.
    * Removed the _required_fields feature and replaced it with jsonschemas

--- gcapi/gcapi.py
+++ gcapi/gcapi.py
@@ -25,43 +25,64 @@ def is_uuid(s):
         return True
 
 
+def import_json_schema(filename):
+    """
+    Loads a json schema from the module's subdirectory "schemas".
+
+    This is not *really* an import but the naming indicates that an ImportError
+    is raised in case the json schema cannot be loaded. This should also only
+    be called while the module is loaded, not at a later stage, because import
+    errors should be raised straight away.
+
+    Parameters
+    ----------
+    filename: str
+        The jsonschema file to be loaded. The filename is relative to the
+        "schemas" directory.
+
+    Returns
+    -------
+    Draft7Validator
+        The jsonschema validation object
+
+    Raises
+    ------
+    ImportError
+        Raised if the json schema cannot be loaded.
+    """
+    filename = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "schemas", filename)
+
+    try:
+        with open(filename, "r") as f:
+            jsn = json.load(f)
+        return jsonschema.Draft7Validator(jsn)
+    except ValueError as e:
+        # I want missing/failing json imports to be an import error because that
+        # is what they should indicate: a "broken" library
+        raise ImportError("Json schema '{file}' cannot be loaded: {error}".format(
+            file=filename, error=e))
+
+
 class APIBase:
     _client = None  # type: Client
     base_path = ""
     sub_apis = {}
 
-    json_schema_file = None
-    __json_schema = None
-
-    @staticmethod
-    def _attempt_load_json_schema(filename):
-        if filename is None:
-            return None
-        filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "schemas", filename
-        )
-
-        try:
-            with open(filename, "r") as f:
-                jsn = json.load(f)
-            return jsonschema.Draft7Validator(jsn)
-        except ValueError as e:
-            print("Error parsing '{}': {}".format(e, filename), file=sys.stderr)
-            return None
+    json_schema = None
 
     def __init__(self, client):
         if isinstance(self, ModifiableMixin):
             ModifiableMixin.__init__(self)
 
         self._client = client
-        self.__json_schema = self._attempt_load_json_schema(self.json_schema_file)
 
         for k, api in self.sub_apis.items():
             setattr(self, k, api(self._client))
 
     def __verify_against_schema(self, value):
-        if self.__json_schema is not None:
-            self.__json_schema.validate(value)
+        if self.json_schema is not None:
+            self.json_schema.validate(value)
 
     def page(self, offset=0, limit=100):
         result = self._client(
@@ -90,20 +111,12 @@ class APIBase:
 
 class ModifiableMixin:
     _client = None  # type: Client
-    _required_fields = []
 
-    modify_json_schema_file = None
-    __json_schema = None
-
-    def __init__(self):
-        self.__json_schema = self._attempt_load_json_schema(
-            self.modify_json_schema_file
-        )
+    modify_json_schema = None # type: jsonschema.Draft7Validator
 
     def _process_post_arguments(self, post_args):
-        for k in self._required_fields:
-            if k not in post_args:
-                raise ValueError("Missing field: {}".format(k))
+        if self.modify_json_schema is not None:
+            self.modify_json_schema.validate(post_args)
 
     def send(self, **kwargs):
         self._process_post_arguments(kwargs)
@@ -128,30 +141,31 @@ class ReaderStudyQuestionsAPI(APIBase):
 
 class ReaderStudyMineAnswersAPI(APIBase, ModifiableMixin):
     base_path = "reader-studies/answers/mine/"
-    json_schema_file = "answer.json"
+    json_schema = import_json_schema("answer.json")
 
 
 class ReaderStudyAnswersAPI(APIBase, ModifiableMixin):
     base_path = "reader-studies/answers/"
-    json_schema_file = "answer.json"
+    json_schema = import_json_schema("answer.json")
+    modify_json_schema = import_json_schema("post-answer.json")
+
     sub_apis = {"mine": ReaderStudyMineAnswersAPI}
-    _required_fields = ("answer", "images", "question")
 
     mine = None  # type: ReaderStudyMineAnswersAPI
 
     def _process_post_arguments(self, post_args):
-        ModifiableMixin._process_post_arguments(self, post_args)
-
         if is_uuid(post_args["question"]):
             post_args["question"] = urljoin(
                 urljoin(self._client.base_url, ReaderStudyQuestionsAPI.base_path),
                 post_args["question"] + "/",
             )
 
+        ModifiableMixin._process_post_arguments(self, post_args)
+
 
 class ReaderStudiesAPI(APIBase):
     base_path = "reader-studies/"
-    json_schema_file = "reader-study.json"
+    json_schema = import_json_schema("reader-study.json")
 
     sub_apis = {"answers": ReaderStudyAnswersAPI, "questions": ReaderStudyQuestionsAPI}
 
@@ -161,8 +175,8 @@ class ReaderStudiesAPI(APIBase):
 
 class Client(Session):
     def __init__(
-        self, token=None, base_url="https://grand-challenge.org/api/v1/", verify=True
-    ):
+            self, token=None, base_url="https://grand-challenge.org/api/v1/",
+            verify=True):
         super(Client, self).__init__()
 
         self.headers.update({"Accept": "application/json"})
