27dcdfdecc7301001f8969ff9dc07d7a7a70254d
Miriam-Groeneveld <Miriam.Groeneveld.Diag@gmail.com>
2019-08-22 14:40:34 +0200

    Merged changes

--- gcapi/gcapi.py
+++ gcapi/gcapi.py
@@ -2,15 +2,10 @@
 from __future__ import print_function, division, absolute_import
 
 from future import standard_library
-standard_library.install_aliases()
 from builtins import object
+import json
 import os
 import uuid
-
-import json
-
-import sys
-
 import jsonschema
 
 try:
@@ -19,6 +14,8 @@ except ImportError:
     from urllib.parse import urljoin
 
 from requests import Session
+
+standard_library.install_aliases()
 
 
 def is_uuid(s):
@@ -30,51 +27,83 @@ def is_uuid(s):
         return True
 
 
+def accept_tuples_as_arrays(org):
+    return org.redefine(
+        "array",
+        lambda checker, instance:
+            isinstance(instance, tuple) or org.is_type(instance, "array"))
+
+
+Draft7ValidatorWithTupleSupport = jsonschema.validators.extend(
+    jsonschema.Draft7Validator,
+    type_checker=accept_tuples_as_arrays(jsonschema.Draft7Validator.TYPE_CHECKER))
+
+
+def import_json_schema(filename):
+    """
+    Loads a json schema from the module's subdirectory "schemas".
+
+    This is not *really* an import but the naming indicates that an ImportError
+    is raised in case the json schema cannot be loaded. This should also only
+    be called while the module is loaded, not at a later stage, because import
+    errors should be raised straight away.
+
+    Parameters
+    ----------
+    filename: str
+        The jsonschema file to be loaded. The filename is relative to the
+        "schemas" directory.
+
+    Returns
+    -------
+    Draft7Validator
+        The jsonschema validation object
+
+    Raises
+    ------
+    ImportError
+        Raised if the json schema cannot be loaded.
+    """
+    filename = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "schemas", filename)
+
+    try:
+        with open(filename, "r") as f:
+            jsn = json.load(f)
+        return  Draft7ValidatorWithTupleSupport(jsn)
+    except ValueError as e:
+        # I want missing/failing json imports to be an import error because that
+        # is what they should indicate: a "broken" library
+        raise ImportError("Json schema '{file}' cannot be loaded: {error}".format(
+            file=filename, error=e))
+
+
 class APIBase(object):
-    _client = None # type: Client
+    _client = None  # type: Client
     base_path = ""
     sub_apis = {}
 
-    json_schema_file = None
-    __json_schema = None
-
-    @staticmethod
-    def _attempt_load_json_schema(filename):
-        if filename is None:
-            return None
-        filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "schemas",
-            filename)
-
-        try:
-            with open(filename, "r") as f:
-                jsn = json.load(f)
-            return jsonschema.Draft7Validator(jsn)
-        except ValueError as e:
-            print("Error parsing '{}': {}".format(
-                e, filename), file=sys.stderr)
-            return None
+    json_schema = None
 
     def __init__(self, client):
         if isinstance(self, ModifiableMixin):
             ModifiableMixin.__init__(self)
 
         self._client = client
-        self.__json_schema = self._attempt_load_json_schema(self.json_schema_file)
 
         for k, api in list(self.sub_apis.items()):
             setattr(self, k, api(self._client))
 
     def __verify_against_schema(self, value):
-        if self.__json_schema is not None:
-            self.__json_schema.validate(value)
+        if self.json_schema is not None:
+            self.json_schema.validate(value)
 
     def page(self, offset=0, limit=100):
         result = self._client(
             method="GET", path=self.base_path, params={"offset": offset, "limit": limit}
         )["results"]
-        for i in result: self.__verify_against_schema(i)
+        for i in result:
+            self.__verify_against_schema(i)
         return result
 
     def iterate_all(self):
@@ -95,20 +124,16 @@ class APIBase(object):
 
 
 class ModifiableMixin(object):
-    _client = None # type: Client
-    _required_fields = []
+    _client = None  # type: Client
 
-    modify_json_schema_file = None
-    __json_schema = None
+    modify_json_schema = None # type: jsonschema.Draft7Validator
 
     def __init__(self):
-        self.__json_schema = self._attempt_load_json_schema(
-            self.modify_json_schema_file)
+        pass
 
     def _process_post_arguments(self, post_args):
-        for k in self._required_fields:
-            if k not in post_args:
-                raise ValueError("Missing field: {}".format(k))
+        if self.modify_json_schema is not None:
+            self.modify_json_schema.validate(post_args)
 
     def send(self, **kwargs):
         self._process_post_arguments(kwargs)
@@ -116,7 +141,7 @@ class ModifiableMixin(object):
             method="POST",
             path=self.base_path,
             json=kwargs,
-            extra_headers={"Content-Type": "application/json"})
+        )
 
 
 class ImagesAPI(APIBase):
@@ -133,46 +158,54 @@ class ReaderStudyQuestionsAPI(APIBase):
 
 class ReaderStudyMineAnswersAPI(APIBase, ModifiableMixin):
     base_path = "reader-studies/answers/mine/"
-    json_schema_file = "answer.json"
+    json_schema = import_json_schema("answer.json")
 
 
 class ReaderStudyAnswersAPI(APIBase, ModifiableMixin):
     base_path = "reader-studies/answers/"
-    json_schema_file = "answer.json"
-    sub_apis = {
-        "mine": ReaderStudyMineAnswersAPI,
-    }
-    _required_fields = ("answer", "images", "question")
+    json_schema = import_json_schema("answer.json")
+    modify_json_schema = import_json_schema("post-answer.json")
 
-    mine = None # type: ReaderStudyMineAnswersAPI
+    sub_apis = {"mine": ReaderStudyMineAnswersAPI}
+
+    mine = None  # type: ReaderStudyMineAnswersAPI
 
     def _process_post_arguments(self, post_args):
-        ModifiableMixin._process_post_arguments(self, post_args)
-
         if is_uuid(post_args["question"]):
-            post_args["question"] = urljoin(urljoin(
-                self._client.base_url,
-                ReaderStudyQuestionsAPI.base_path
-            ),  post_args["question"] + "/")
+            post_args["question"] = urljoin(
+                urljoin(self._client.base_url, ReaderStudyQuestionsAPI.base_path),
+                post_args["question"] + "/",
+            )
+
+        ModifiableMixin._process_post_arguments(self, post_args)
 
 
 class ReaderStudiesAPI(APIBase):
     base_path = "reader-studies/"
-    json_schema_file = "reader-study.json"
+    json_schema = import_json_schema("reader-study.json")
 
-    sub_apis = {
-        "answers": ReaderStudyAnswersAPI,
-        "questions": ReaderStudyQuestionsAPI,
-    }
+    sub_apis = {"answers": ReaderStudyAnswersAPI, "questions": ReaderStudyQuestionsAPI}
 
-    answers = None # type: ReaderStudyAnswersAPI
-    questions = None # type: ReaderStudyQuestionsAPI
+    answers = None  # type: ReaderStudyAnswersAPI
+    questions = None  # type: ReaderStudyQuestionsAPI
+
+
+class AlgorithmsAPI(APIBase):
+    base_path = "algorithms/"
+
+
+class AlgorithmResultsAPI(APIBase):
+    base_path = "algorithms/results/"
+
+
+class AlgorithmJobsAPI(APIBase):
+    base_path = "algorithms/jobs/"
 
 
 class Client(Session):
     def __init__(
-        self, token=None, base_url="https://grand-challenge.org/api/v1/", verify=True
-    ):
+            self, token=None, base_url="https://grand-challenge.org/api/v1/",
+            verify=True):
         super(Client, self).__init__()
 
         self.headers.update({"Accept": "application/json"})
@@ -192,6 +225,9 @@ class Client(Session):
         self.images = ImagesAPI(client=self)
         self.reader_studies = ReaderStudiesAPI(client=self)
         self.sessions = WorkstationSessionsAPI(client=self)
+        self.algorithms = AlgorithmsAPI(client=self)
+        self.algorithm_results = AlgorithmResultsAPI(client=self)
+        self.algorithm_jobs = AlgorithmJobsAPI(client=self)
 
     @property
     def base_url(self):
@@ -201,9 +237,16 @@ class Client(Session):
         if not url.startswith(self._base_url):
             raise RuntimeError("{} does not start with {}".format(url, self._base_url))
 
-    def __call__(self, method="GET", url="", path="", params=None, json=None, extra_headers={}):
+    def __call__(self,
+            method="GET", url="", path="", params=None,
+            json=None, extra_headers=None):
         if not url:
             url = urljoin(self._base_url, path)
+        if extra_headers is None:
+            extra_headers = {}
+        if json is not None:
+            extra_headers["Content-Type"] = "application/json"
+
         self._validate_url(url)
 
         response = self.request(
