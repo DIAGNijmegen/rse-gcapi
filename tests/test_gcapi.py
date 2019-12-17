57c3424ed801618259d3e4f2e385591e87592806
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-12-17 18:25:15 +0100

    Add datetime format test

--- tests/test_gcapi.py
+++ tests/test_gcapi.py
@@ -7,6 +7,7 @@ import os
 import pytest
 
 from click.testing import CliRunner
+from jsonschema import ValidationError
 from requests.exceptions import HTTPError
 
 from gcapi import Client
@@ -92,6 +93,60 @@ def test_local_response():
     )
     # Empty response, but it didn't error out so the server is responding
     assert c.algorithms.page() == []
+
+
+@pytest.mark.parametrize(
+    "datetime_string,valid",
+    (
+        ("teststring", False),
+        (1, False),
+        ({}, False),
+        ("2019-13-11T13:55:00.123456Z", False),
+        ("2019-12-11T25:55:00.123456Z", False),
+        ("2019-12-11T13:60:00.123456Z", False),
+        ("2019-12-11T13:55:00.123456Z", True),
+        ("2019-12-11T13:55:00Z", True),
+    ),
+)
+def test_datetime_string_format_validation(datetime_string, valid):
+    landmark_annotation = {
+        "id": "4d5721f8-485d-4a17-8507-e06a8f897dd3",
+        "grader": 7,
+        "created": datetime_string,
+        "singlelandmarkannotation_set": [
+            {
+                "id": "69ea96c2-9a96-4080-9b2c-0b9d0417dda1",
+                "image": "70ec13fd-7fcf-4c84-bcd0-5fa3ac34a6b0",
+                "landmarks": [
+                    [249.029700179422, 194.950491966439],
+                    [308.910901038675, 210.158411188267],
+                    [270.891081357472, 353.683160558702],
+                ],
+            },
+            {
+                "id": "8055d041-d78d-4fa6-94af-1e57e80e11d8",
+                "image": "46ba46f4-7fcd-418d-a43f-f668b286daeb",
+                "landmarks": [
+                    [759.567661360211, 495.505389057573],
+                    [911.79182925945, 646.176269350096],
+                    [672.582428997143, 726.948261175343],
+                ],
+            },
+        ],
+    }
+    c = Client(
+        base_url="https://gc.localhost/api/v1/",
+        verify=False,
+        token="f1f98a1733c05b12118785ffd995c250fe4d90da",  # retina token
+    )
+    if valid:
+        assert (
+            c.retina_landmark_annotations._verify_against_schema(landmark_annotation)
+            is None
+        )
+    else:
+        with pytest.raises(ValidationError):
+            c.retina_landmark_annotations._verify_against_schema(landmark_annotation)
 
 
 def test_list_landmark_annotations():
