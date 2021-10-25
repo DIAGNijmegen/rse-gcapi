d543247e0967aaba1d3487d2f017be9683302c59
Paul Konstantin Gerke <pkcakeout@gmail.com>
2021-10-25 19:46:32 +0200

    Start adding docstrings

--- gcapi/sync_async_hybrid_support.py
+++ gcapi/sync_async_hybrid_support.py
@@ -1,3 +1,19 @@
+"""
+This module contains definitions that allow functions to expose a
+"hybrid implementation" that works for classes that should expose a synchronous
+or asynchrnous interface.
+
+Problem
+-------
+
+TODO
+
+Solution
+--------
+
+TODO
+"""
+
 from typing import Callable, Dict, NamedTuple, Tuple, Union
 
 
@@ -9,12 +25,25 @@ class _CapturedCall(NamedTuple):
 
 # Paul K.: Trick to get the SLOT-constant through the mypy verification, which
 # does not like extra members being added to the a NamedTuple. dataclasses
-# would be the correct answer, but are not availabe in python 3.6 which we need
+# would be the correct answer, but are not available in python 3.6 which we need
 # for MeVisLab
 class CapturedCall(_CapturedCall):
+    """
+    A "CapturedCall" is a description of a "deep call" including all arguments.
+    A "deep call" is a call to a function in some complex object-hierarchy,
+    like:
+
+    `parent.child.grandchild.func("a", "b")`
+    """
+
     SLOT = object()
 
     def execute(self, root):
+        """
+        Use this member function to execute a call using `root` as the
+        new object to defer the captured call to.
+        """
+
         def sub(x):
             if x is CapturedCall.SLOT:
                 return root
@@ -30,6 +59,20 @@ class CapturedCall(_CapturedCall):
 
 
 class CallCapture:
+    """
+    This class can be used to "capture" a call. This means, it is capturing all
+    arguments for a complex member-call to a deep object structure like:
+
+    `parent.child.grandchild.func("a", "b")`
+
+    Capturing a call through this function, will yield a "CapturedCall", which
+    can be executed in some other context using the CapturedCall.execute
+    function.
+
+    This is used to defer calling a function in hybrid sync/async functions
+    to a callee which knows which interface (sync/async) to use for the call.
+    """
+
     ref_self: Union[object, CapturedCall]
 
     def __init__(self, ref_self=CapturedCall.SLOT):
@@ -55,10 +98,21 @@ GENERATOR_MARKER = "__gcapi_generator"
 
 
 def mark_generator(f):
+    """
+    All types of functions (generator or normal) that should support hybrid
+    operations (sync and async) will have a generatorfunction-like signature.
+
+    It is therefore necessary to mark generators that should get a
+    generator signature anyway, with some other means. This function can be
+    used as a decorator to mark those functions
+    """
     setattr(f, GENERATOR_MARKER, True)
     return f
 
 
 def is_generator(f):
+    """
+    This is the buddy of "mark_generator" and checks if a function is marked
+    as a generator.
+    """
     return getattr(f, GENERATOR_MARKER, False)
-
