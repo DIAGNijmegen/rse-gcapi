1c8c968a31eb2e4a7db55d08767ebef806d0e636
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-10-28 17:04:42 +0200

    Match endpoint requirements

--- gcapi/client.py
+++ gcapi/client.py
@@ -567,9 +567,9 @@ class ClientBase(ApiDefinitions, ClientInterface):
         if answer is not None:
             upload_session_data["answer"] = answer
 
-        if len(upload_session_data) != 1:
+        if len(upload_session_data) > 1:
             raise ValueError(
-                "One of archive, reader_study or answer should be set"
+                "Only one of archive, answer or reader_study can be set"
             )
 
         raw_image_upload_session = yield from self._upload_files(
