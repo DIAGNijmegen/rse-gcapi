7d26f55fcb6dbd12b92e4724e8dc11a60aad3206
Chris <chris.vanrun@radboudumc.nl>
2022-04-07 12:37:44 +0200

    Fix not being able to use str as file-like object when uploading

--- gcapi/client.py
+++ gcapi/client.py
@@ -578,7 +578,7 @@ class ClientBase(ApiDefinitions, ClientInterface):
                 uploads.append(
                     (
                         yield from self.__org_api_meta.uploads.upload_fileobj(
-                            fileobj=f, filename=file.name
+                            fileobj=f, filename=f.name
                         )
                     )
                 )
