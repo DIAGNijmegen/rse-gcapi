bb3877e89392311c9a45a3fe3d1cd05883a84ad0
miriam <miriam.groeneveld.diag@gmail.com>
2022-05-30 17:15:21 +0200

    black

--- gcapi/client.py
+++ gcapi/client.py
@@ -419,7 +419,7 @@ class UploadsAPI(APIBase):
                 if status_code in [409, 423] or status_code >= 500:
                     num_retries += 1
                     e = _e
-                    sleep((2**num_retries) + (randint(0, 1000) / 1000))
+                    sleep((2 ** num_retries) + (randint(0, 1000) / 1000))
                 else:
                     raise
         else:
@@ -909,9 +909,7 @@ class ClientBase(ApiDefinitions, ClientInterface):
                     ) from e
 
                 yield from self._upload_files(
-                    display_set=ds["pk"],
-                    interface=interface,
-                    files=files
+                    display_set=ds["pk"], interface=interface, files=files
                 )
             res.append(ds["pk"])
         return res  # noqa: B901
