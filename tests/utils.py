e71ddba13ad33ea65e1df8f923bb5fd1096e451c
miriam <miriam.groeneveld.diag@gmail.com>
2022-05-31 16:49:31 +0200

    created function for retrying queries

---
+++ tests/utils.py
@@ -0,0 +1,32 @@
+from httpx import HTTPStatusError
+from time import sleep
+
+
+def recurse_call(func):
+	def wrapper(*args, **kwargs):
+		for _ in range(60):
+			try:
+				result = func(*args, **kwargs)
+				break
+			except (HTTPStatusError, ValueError):
+				sleep(0.5)
+		else:
+			raise TimeoutError
+		return result
+
+	return wrapper
+
+
+def async_recurse_call(func):
+	async def wrapper(*args, **kwargs):
+		for _ in range(60):
+			try:
+				result = await func(*args, **kwargs)
+				break
+			except (HTTPStatusError, ValueError):
+				sleep(0.5)
+		else:
+			raise TimeoutError
+		return result
+
+	return wrapper
