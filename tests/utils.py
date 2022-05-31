e0d45317cd1565117364772304d08ee7906330ab
miriam <miriam.groeneveld.diag@gmail.com>
2022-05-31 17:15:41 +0200

    black

--- tests/utils.py
+++ tests/utils.py
@@ -3,30 +3,30 @@ from time import sleep
 
 
 def recurse_call(func):
-	def wrapper(*args, **kwargs):
-		for _ in range(60):
-			try:
-				result = func(*args, **kwargs)
-				break
-			except (HTTPStatusError, ValueError):
-				sleep(0.5)
-		else:
-			raise TimeoutError
-		return result
+    def wrapper(*args, **kwargs):
+        for _ in range(60):
+            try:
+                result = func(*args, **kwargs)
+                break
+            except (HTTPStatusError, ValueError):
+                sleep(0.5)
+        else:
+            raise TimeoutError
+        return result
 
-	return wrapper
+    return wrapper
 
 
 def async_recurse_call(func):
-	async def wrapper(*args, **kwargs):
-		for _ in range(60):
-			try:
-				result = await func(*args, **kwargs)
-				break
-			except (HTTPStatusError, ValueError):
-				sleep(0.5)
-		else:
-			raise TimeoutError
-		return result
+    async def wrapper(*args, **kwargs):
+        for _ in range(60):
+            try:
+                result = await func(*args, **kwargs)
+                break
+            except (HTTPStatusError, ValueError):
+                sleep(0.5)
+        else:
+            raise TimeoutError
+        return result
 
-	return wrapper
+    return wrapper
