9aceeb639cde93850fc593c7675d5eca11c3d110
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2024-08-07 15:55:35 +0200

    Add DeprecationWarnings for item getting, setting and deleting

--- gcapi/model_base.py
+++ gcapi/model_base.py
@@ -1,9 +1,27 @@
+import warnings
+
+
 class BaseModel:
     def __getitem__(self, key):
+        self._warn_deprecated_access(key, "getting")
         return getattr(self, key)
 
     def __setitem__(self, key, value):
+        self._warn_deprecated_access(key, "setting")
         return setattr(self, key, value)
 
     def __delitem__(self, key):
+        self._warn_deprecated_access(key, "deleting")
         return delattr(self, key)
+
+    @staticmethod
+    def _warn_deprecated_access(key, action):
+        warnings.warn(
+            message=(
+                f'Using ["{key}"] for {action} attributes is deprecated '
+                "and will be removed in the next release. "
+                f'Suggestion: Replace ["{key}"] with .{key}'
+            ),
+            category=DeprecationWarning,
+            stacklevel=3,
+        )
