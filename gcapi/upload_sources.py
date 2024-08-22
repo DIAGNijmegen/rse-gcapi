73e2f76884270240bc84351e52347f2afefbc86f
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-08-22 10:46:09 +0200

    Minor refactor of upload sources and allow for values as input to file-super_kind interfaces

--- gcapi/upload_sources.py
+++ gcapi/upload_sources.py
@@ -1,8 +1,16 @@
+import io
 import json
 from pathlib import Path
 from typing import Any, Optional, Union
 
-from gcapi.models import ComponentInterface, SimpleImage
+from httpx import AsyncClient, Client
+
+from gcapi.models import (
+    ComponentInterface,
+    ComponentInterfaceValuePostRequest,
+    HyperlinkedImage,
+    SimpleImage,
+)
 
 # First, wrap every potential CIV in a call that has a basic validation call
 
@@ -24,67 +32,160 @@ class TooManyFiles(ValueError):
 
 
 def interface_to_civ_source(interface: ComponentInterface):
-    return {
-        "Image": ImageCIVSource,
-        "File": FileCIVSource,
-        "Value": ValueCIVSource,
-    }[interface.super_kind]
+    handler = {
+        "image": ImageCIVSource,
+        "file": FileCIVSource,
+        "value": ValueCIVSource,
+    }[interface.super_kind.casefold()]
+
+    return handler
 
 
 FileSource = Union[Path, list[Path], str, list[str]]
 
 
-class FileCIVSource:
-    max_num_sources: Optional[int] = 1
+class BaseCIVSource:
+    interface: ComponentInterface
+    client: Union[Client, AsyncClient]
 
-    def __init__(self, source: FileSource):
-        sources = [source] if not isinstance(source, list) else source
+    def __init__(self, interface, client):
+        self.interface = interface
+        self.client = client
 
-        if (
-            self.max_num_sources is not None
-            and len(sources) > self.max_num_sources
-        ):
-            raise TooManyFiles(
-                f"Only {self.max_num_sources} are supported,"
-                "you provided {len(sources)}"
-            )
-
-        self.content = self._validate_file_sources(sources)
-
-    def _validate_file_sources(self, sources):
-        validated = []
-        for s in sources:
-            path = Path(s) if isinstance(s, (str, Path)) else None
-            if path and path.exists():
-                validated.append(path)
-            else:
-                raise FileNotFoundError(s)
-        return validated
+    def process(self) -> ComponentInterfaceValuePostRequest:
+        return ComponentInterfaceValuePostRequest(
+            interface=self.interface.slug,
+            value=None,
+            file=None,
+            image=None,
+            upload_session=None,
+            user_upload=None,
+        )
 
 
-class ImageCIVSource(FileCIVSource):
-    max_num_sources = None
+def clean_file_source(
+    source: FileSource, maximum_number: Optional[int] = None
+):
+    sources = [source] if not isinstance(source, list) else source
 
-    simple_image = None
-
-    def __init__(self, source: Union[FileSource, SimpleImage]):
-        if isinstance(source, SimpleImage):
-            self.content = source
+    validated = []
+    for s in sources:
+        path = Path(s) if isinstance(s, (str, Path)) else None
+        if path and path.exists():
+            validated.append(path)
         else:
-            super().__init__(source)
+            raise FileNotFoundError(s)
+
+    if maximum_number is not None and len(sources) > maximum_number:
+        raise TooManyFiles(
+            f"The maximum is {maximum_number}, "
+            f"you provided {maximum_number}: {sources}"
+        )
+
+    return validated
 
 
-class ValueCIVSource(FileCIVSource):
-    max_num_sources = 1
+class FileCIVSource(BaseCIVSource):
+    def __init__(self, source: FileSource, **kwargs):
+        super().__init__(**kwargs)
 
-    def __init__(self, source: Union[FileSource, Any]):
         try:
-            super().__init__(source)
+            cleaned_sources = clean_file_source(source, maximum_number=1)
+            self.content = cleaned_sources[0]
+            self.content_name = self.content.name
+        except FileNotFoundError as file_val_error:
+            # Possibly a directly provided value
+            if self.interface.kind.casefold() == "string":
+                # Incorrect paths being uploaded as content to a string interface can
+                # easily bypass detection because:
+                #   - Platform JSON schemas for strings are typically underdefined
+                #   - Files with super_kind require users to download them before
+                #    viewing the content.
+                raise FileNotFoundError(
+                    f"Interface kind {self.interface.kind} requires to be upload "
+                    "as a file, replace value with an existing file path"
+                ) from file_val_error
+            try:
+                json_str = json.dumps(source)
+            except TypeError:
+                raise file_val_error
+            self.content = io.StringIO(json_str)
+            self.content_name = self.interface.relative_path
+
+    def process(self):
+        post = super().process()
+
+        with open(self.content, "rb") as f:
+            user_upload = yield from self.client.uploads.upload_fileobj(
+                fileobj=f, filename=self.content_name
+            )
+        post.user_upload = user_upload.api_url
+
+        return post
+
+
+class ImageCIVSource(BaseCIVSource):
+
+    def __init__(
+        self,
+        source: Union[FileSource, SimpleImage, HyperlinkedImage],
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        if isinstance(source, (SimpleImage, HyperlinkedImage)):
+            self.content = self.client.images.detail(pk=source.pk)
+        else:
+            self.content = clean_file_source(source)
+
+    def process(self):
+        post = super().process()
+
+        if isinstance(self.content, HyperlinkedImage):
+            # Reuse the existing image
+            post.image = self.content.api_url
+            return post
+
+        # Upload the image
+        uploads = []
+        for file in self.content:
+            with open(file, "rb") as f:
+                uploads.append(
+                    (
+                        yield from self.client.uploads.upload_fileobj(
+                            fileobj=f, filename=file.name
+                        )
+                    )
+                )
+
+        raw_image_upload = (
+            yield from self.client.raw_image_upload_sessions.create(
+                uploads=[u.api_url for u in uploads],
+            )
+        )
+
+        post.upload_session = raw_image_upload.api_url
+
+        return post
+
+
+class ValueCIVSource(BaseCIVSource):
+    def __init__(self, source: Union[FileSource, Any], **kwargs):
+        super().__init__(**kwargs)
+
+        try:
+            cleaned_sources = clean_file_source(source, maximum_number=1)
+            clean_source = cleaned_sources[0]
         except FileNotFoundError:
-            # Directly provided value
+            # Directly provided value?
             json.dumps(source)  # Check if it is JSON serializable
             self.content = source
         else:
-            # A singular json file, read and parse the content
-            with open(self.content[0]) as fp:
+            # A singular json file
+            with open(clean_source) as fp:
                 self.content = json.load(fp)
+
+    def process(self):
+        data = super().process()
+        data.value = self.content
+        return data
--- tests/test_upload_sources.py
+++ tests/test_upload_sources.py
@@ -8,31 +8,99 @@ from gcapi.upload_sources import (
     ImageCIVSource,
     TooManyFiles,
     ValueCIVSource,
+    clean_file_source,
 )
-from tests.factories import SimpleImageFactory
+from tests.factories import ComponentInterfaceFactory, SimpleImageFactory
 
 TESTDATA = Path(__file__).parent / "testdata"
+
+from unittest.mock import MagicMock
+
+
+@pytest.mark.parametrize(
+    "source,max_number,context",
+    (
+        (
+            TESTDATA / "test.json",
+            None,
+            nullcontext(),
+        ),
+        (
+            [TESTDATA / "test.json"],
+            None,
+            nullcontext(),
+        ),
+        (
+            str(TESTDATA / "test.json"),
+            None,
+            nullcontext(),
+        ),
+        (
+            [str(TESTDATA / "test.json")],
+            None,
+            nullcontext(),
+        ),
+        (
+            [TESTDATA / "test.json", TESTDATA / "test.json"],
+            1,
+            pytest.raises(TooManyFiles),
+        ),
+        (
+            "I DO NOT EXIST",
+            None,
+            pytest.raises(FileNotFoundError),
+        ),
+        (
+            ["I DO NOT EXIST"],
+            None,
+            pytest.raises(FileNotFoundError),
+        ),
+    ),
+)
+def test_clean_file_source(source, max_number, context):
+    with context:
+        clean_file_source(source, max_number)
 
 
 @pytest.mark.parametrize(
     "source,context",
     (
         (TESTDATA / "test.json", nullcontext()),
-        ([TESTDATA / "test.json"], nullcontext()),
-        (str(TESTDATA / "test.json"), nullcontext()),
-        ([str(TESTDATA / "test.json")], nullcontext()),
         (
             [TESTDATA / "test.json", TESTDATA / "test.json"],
             pytest.raises(TooManyFiles),
         ),
-        ("I DO NOT EXIST", pytest.raises(FileNotFoundError)),
-        (["I DO NOT EXIST"], pytest.raises(FileNotFoundError)),
-        (["I DO NOT EXIST", "I DO NOT EXIST"], pytest.raises(TooManyFiles)),
     ),
 )
 def test_file_source_validation(source, context):
     with context:
-        FileCIVSource(source)
+        FileCIVSource(
+            source,
+            interface=ComponentInterfaceFactory(super_kind="File"),
+            client=MagicMock(),
+        )
+
+
+@pytest.mark.parametrize(
+    "source,context,interface_kind",
+    (
+        ({"foo": "bar"}, nullcontext(), "Anything"),
+        (
+            "A string which is not a file",
+            pytest.raises(FileNotFoundError),
+            "String",
+        ),
+    ),
+)
+def test_value_on_file_source(source, context, interface_kind):
+    with context:
+        FileCIVSource(
+            source,
+            interface=ComponentInterfaceFactory(
+                super_kind="File", kind=interface_kind
+            ),
+            client=MagicMock(),
+        )
 
 
 @pytest.mark.parametrize(
@@ -40,15 +108,22 @@ def test_file_source_validation(source, context):
     (
         (TESTDATA / "image10x10x101.mha", nullcontext()),
         (
-            [TESTDATA / "image10x10x10.mhd", TESTDATA / "image10x10x10.zraw"],
+            [
+                TESTDATA / "image10x10x10.mhd",
+                TESTDATA / "image10x10x10.zraw",
+            ],
             nullcontext(),
         ),
         (SimpleImageFactory(), nullcontext()),
     ),
 )
-def test_image_file_source_validation(source, context):
+def test_image_source_validation(source, context):
     with context:
-        ImageCIVSource(source)
+        ImageCIVSource(
+            source,
+            interface=ComponentInterfaceFactory(super_kind="Image"),
+            client=MagicMock(),
+        )
 
 
 @pytest.mark.parametrize(
@@ -57,15 +132,24 @@ def test_image_file_source_validation(source, context):
         (TESTDATA / "test.json", nullcontext()),
         ([TESTDATA / "test.json"], nullcontext()),
         (
-            [TESTDATA / "test.json", TESTDATA / "test.json"],
+            [
+                TESTDATA / "test.json",
+                TESTDATA / "test.json",
+            ],
             pytest.raises(TooManyFiles),
         ),
         (TESTDATA / "invalid_test.json", pytest.raises(ValueError)),
         ({"foo": "bar"}, nullcontext()),
+        (["foo", "bar"], nullcontext()),
         (1, nullcontext()),
+        (None, nullcontext()),
         (object(), pytest.raises(TypeError)),
     ),
 )
 def test_value_source_validation(source, context):
     with context:
-        ValueCIVSource(source)
+        ValueCIVSource(
+            source,
+            interface=ComponentInterfaceFactory(super_kind="Value"),
+            client=MagicMock(),
+        )
