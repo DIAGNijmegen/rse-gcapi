45a0cd4c271c57d4979722b95bcc9819fd50b431
Chris van Run <chrisvanrun@users.noreply.github.com>
2025-06-03 10:36:19 +0200

    Remove AsyncClient (#207)
    First part of:
    - https://github.com/DIAGNijmegen/rse-gcapi/issues/164
    
    Splitting the changeset as much as possible.

--- gcapi/__init__.py
+++ gcapi/__init__.py
@@ -1,5 +1,3 @@
-from gcapi.gcapi import AsyncClient, Client
+from gcapi.gcapi import Client
 
-__author__ = """James Meakin"""
-__email__ = "code@jmsmkn.com"
-__all__ = ["AsyncClient", "Client"]
+__all__ = ["Client"]
