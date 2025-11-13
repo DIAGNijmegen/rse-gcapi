a36feb65b488359bd53044ed5cefba8b743f631e
Chris van Run <chris.vanrun@radboudumc.nl>
2025-11-13 13:49:52 +0100

    Fix spelling

--- docs/examples/upload_retry_strategy.py
+++ docs/examples/upload_retry_strategy.py
@@ -6,7 +6,7 @@ import gcapi
 class UploadRetryStrategy(gcapi.retries.SelectiveBackoffStrategy):
     """Retry strategy that handles upload limits specifically.
 
-    In addition to the standard retriable errors handled by
+    In addition to the standard retryable errors handled by
     SelectiveBackoffStrategy, this strategy also retries on
     BAD_REQUEST responses that indicate the user has created too many
     uploads.
