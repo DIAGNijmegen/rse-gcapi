3ece5bc787529dea432cb5729fcf4bd14750813f
Chris van Run <chris.vanrun@radboudumc.nl>
2025-11-20 15:54:56 +0100

    Make ReadableBuffer simpler

--- gcapi/typing.py
+++ gcapi/typing.py
@@ -6,9 +6,5 @@ import gcapi.models
 class ReadableBuffer(Protocol):
     def read(self, size: int = ..., /) -> bytes: ...
 
-    def seek(self, offset: int, whence: int = ..., /) -> int: ...
-
-    def tell(self) -> int: ...
-
 
 SocketValuePostSet = gcapi.models.DisplaySetPost | gcapi.models.ArchiveItemPost
